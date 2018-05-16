8c4a4260c117faabf37d0636b7a6b6fe6a4a4a2d
joannarogalska <rogalska.joasia@gmail.com>
2018-05-16 12:32:37 +0200

    File System task 1.7 fix

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var fs = require('fs');
 
 fs.readdir('/', function(err, items) {
+    console.log('test',items);
+
+    var fileContent = 'To jest zawartość katalogu:\n';
 
     for (var i=0; i<items.length; i++) {
-        fs.writeFile('./tekst.txt', 'To jest zawartość katalogu:\n' + items, function(err) {
-            if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
-        });
+        fileContent += items[i] + '\n';
     }
+
+    fs.writeFile('./tekst.txt',  fileContent, function(err) {
+        if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
+    });
 });
